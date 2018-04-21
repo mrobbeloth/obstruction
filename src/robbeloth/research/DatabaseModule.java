f05faeb8fe9fd34c60c23d929e7a312b02720d4b
Michael Robbeloth <michael@robbeloth.com>
2018-04-21 14:40:37 -0400

    Make sure only distinct instances are returned from the database when getting the model filenames as entries are by segment

--- src/robbeloth/research/DatabaseModule.java
+++ src/robbeloth/research/DatabaseModule.java
@@ -79,7 +79,7 @@ import org.opencv.core.Point;
 									 " WHERE MOMENTX=? AND MOMENTY=?";  
 	private static String selectccStart = "SELECT " + FILENAME_COLUMN + " FROM " + databaseTableName +
 										  " WHERE " + STARTCCX_COLUMN + "=? AND " + STARTCCY_COLUMN + "=?";
-	private static String selectModelFilenames = "SELECT " + FILENAME_COLUMN + " FROM " + databaseTableName;
+	private static String selectModelFilenames = "SELECT DISTINCT " + FILENAME_COLUMN + " FROM " + databaseTableName;
 	private static volatile DatabaseModule singleton = null;
 	private static final String TABLE_NAME = "TABLE_NAME";
 	/* It really is TABLE_SCHEM for TABLE_SCHEMA*/
